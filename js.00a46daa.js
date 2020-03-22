bfa4c375351f0ebbbf77d2927fb25ded328a85c5
Claudinei Bido <bido.c@live.com>
2020-03-22 20:28:23 +0100

    working on responsive part

--- js.00a46daa.js
+++ js.00a46daa.js
@@ -12161,7 +12161,7 @@ var Fade = /*#__PURE__*/function (_Highway$Transition) {
       }).fromTo(to, 0.5, {
         height: "2vh"
       }, {
-        height: "90vh",
+        height: "80vh",
         top: "10%",
         onComplete: function onComplete() {
           from.remove();
@@ -12229,7 +12229,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38141" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46299" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
